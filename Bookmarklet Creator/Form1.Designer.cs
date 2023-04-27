9fdd880304fc084f71c562ae7e07813e92570764
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-26 15:15:29 -0900

    Add files via upload

---
+++ Bookmarklet Creator/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Bookmarklet_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Textboxinput = new System.Windows.Forms.TextBox();
+            this.textboxoutput = new System.Windows.Forms.TextBox();
+            this.Outputgb = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsTxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Outputgb.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Textboxinput
+            // 
+            this.Textboxinput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Textboxinput.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Textboxinput.Location = new System.Drawing.Point(12, 97);
+            this.Textboxinput.MaxLength = 9999998;
+            this.Textboxinput.Multiline = true;
+            this.Textboxinput.Name = "Textboxinput";
+            this.Textboxinput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Textboxinput.Size = new System.Drawing.Size(444, 452);
+            this.Textboxinput.TabIndex = 0;
+            this.Textboxinput.TextChanged += new System.EventHandler(this.Textboxinput_TextChanged);
+            // 
+            // textboxoutput
+            // 
+            this.textboxoutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textboxoutput.Cursor = System.Windows.Forms.Cursors.No;
+            this.textboxoutput.Location = new System.Drawing.Point(468, 97);
+            this.textboxoutput.MaxLength = 1000000;
+            this.textboxoutput.Multiline = true;
+            this.textboxoutput.Name = "textboxoutput";
+            this.textboxoutput.ReadOnly = true;
+            this.textboxoutput.Size = new System.Drawing.Size(239, 452);
+            this.textboxoutput.TabIndex = 1;
+            // 
+            // Outputgb
+            // 
+            this.Outputgb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Outputgb.Controls.Add(this.button1);
+            this.Outputgb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Outputgb.Location = new System.Drawing.Point(462, 33);
+            this.Outputgb.Name = "Outputgb";
+            this.Outputgb.Size = new System.Drawing.Size(252, 524);
+            this.Outputgb.TabIndex = 2;
+            this.Outputgb.TabStop = false;
+            this.Outputgb.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy to Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(444, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Re-Compile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 65);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            70,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(395, 21);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Font Size";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsTxtToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsTxtToolStripMenuItem
+            // 
+            this.saveAsTxtToolStripMenuItem.Name = "saveAsTxtToolStripMenuItem";
+            this.saveAsTxtToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.saveAsTxtToolStripMenuItem.Text = "Save as Javascript";
+            this.saveAsTxtToolStripMenuItem.Click += new System.EventHandler(this.saveAsTxtToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.saveToolStripMenuItem.Text = "Save bookmarklet";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(719, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textboxoutput);
+            this.Controls.Add(this.Outputgb);
+            this.Controls.Add(this.Textboxinput);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Name = "Form1";
+            this.Text = "Bookmarklet Compiler";
+            this.Outputgb.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Textboxinput;
+        private System.Windows.Forms.TextBox textboxoutput;
+        private System.Windows.Forms.GroupBox Outputgb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsTxtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Bookmarklet Creator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
+using System.CodeDom;
+
+namespace Bookmarklet_Creator
+{
+    public partial class Form1 : Form
+    {
+        void encoder()
+        {
+            str = str.Replace(";", "%3B");
+            str = str.Replace("\n", "");
+            str = Regex.Replace(str, @"\s+", " ");
+        }
+        string final;
+        bool New = false;
+        string str;
+        public Form1()
+        {
+            InitializeComponent();
+            final = Textboxinput.Text = "Delete this & Enter Javascript code here...";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textboxoutput.Text = "";
+            str = Textboxinput.Text;
+            encoder();
+            final = textboxoutput.Text = "(function()%7B" + str + "%7D)()%3B";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (New == false)
+            {
+                New = true;
+                MessageBox.Show("Please note when you try to apply your bookmark to your browser page, you must type in 'javascript:' without the apostrophes in front of the code you paste", "Please Note...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Clipboard.SetText(final);
+            }
+            else
+            {
+                Clipboard.SetText(final);
+            }
+        }
+
+        private void Textboxinput_TextChanged(object sender, EventArgs e)
+        {
+            str = Textboxinput.Text;
+            encoder();
+            //final = textboxoutput.Text = "" + str + "";
+            final = textboxoutput.Text = "(function()%7B" + str + "%7D)()%3B";
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            float fontSize = (float)numericUpDown1.Value;
+            Textboxinput.Font = new Font(Textboxinput.Font.FontFamily, fontSize, Textboxinput.Font.Style);
+        }
+
+        private void saveAsTxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Title = "Pick file location";
+            saveFileDialog1.Filter = "Javascript .js|*.js";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, Textboxinput.Text);
+            }
+
+
+
+            //string username = Environment.UserName;
+            //string filePath = $@"C:\Users\{username}\Desktop\Javascript.js";
+            //File.WriteAllText(filePath, Textboxinput.Text);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Pick file location";
+            saveFileDialog1.Filter = "Text File .txt|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, Textboxinput.Text);
+
+            }
+        }
+    }
+}
+
